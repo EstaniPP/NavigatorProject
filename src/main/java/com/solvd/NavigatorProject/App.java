f0004d2a2aee5cb4f58db82c1e99fb8a08485161
Estanipp <estanipp@gmail.com>
2020-06-05 08:45:22 -0300

    First commit

---
+++ src/main/java/com/solvd/NavigatorProject/App.java
@@ -0,0 +1,13 @@
+package com.solvd.NavigatorProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
